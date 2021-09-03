1b3e932d6a748fec4c328b9c2a2ff419c886a745
lucassmelloo <lucasmellovieira99@gmail.com>
2021-09-02 21:21:17 -0300

    Adicionando a função de maximizar a tela

--- authomation.py
+++ authomation.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 
 class WebController:
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--kiosk")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get('http://portal.labapoiobrasil.com.br/painel/abrir?id=6&token=Nv2KUvjBg8oaUE0BkCNfzgIKe6arAQsk')
     e = driver.find_element_by_xpath("//*[contains(text(),'Abrir Caixas')]")
     e.click()
