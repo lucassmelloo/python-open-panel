94ba0c2a430b8469170f38216db9ac4591ed47b3
lucassmelloo <lucasmellovieira99@gmail.com>
2021-09-16 22:14:08 -0300

    Acrescentando delay

--- authomation.py
+++ authomation.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-
 class WebController:
     chrome_options = Options()
     chrome_options.add_argument("--kiosk")
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get('http://portal.labapoiobrasil.com.br/painel/abrir?id=6&token=Nv2KUvjBg8oaUE0BkCNfzgIKe6arAQsk')
+    driver.implicitly_wait(10)
     e = driver.find_element_by_xpath("//*[contains(text(),'Abrir Caixas')]")
     e.click()
